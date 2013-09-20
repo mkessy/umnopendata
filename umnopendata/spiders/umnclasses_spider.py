cd82f5d7319793f05e8b0b315298d7b6ee320784
Mepuka Kessy <kessy002@umn.edu>
2013-09-19 23:41:44 -0500

    regexs for class description parsing

--- umnopendata/spiders/umnclasses_spider.py
+++ umnopendata/spiders/umnclasses_spider.py
@@ -5,6 +5,7 @@ from scrapy.http import Request, FormRequest
 from umnopendata.items import ClassItem, DepartmentItem
 
 from itertools import product
+import re
 
 class UMNClassesSpider(BaseSpider):
     """
@@ -25,6 +26,29 @@ classschedule_selectsubject.jsp?campus=UMNTC']
         """
         return Request(url=url, callback=self.parse_schedule_form_options)
 
+    def parse_class_description(desc):
+        """
+        Parse a course description returning the following class parameters
+        in dictionary format.
+        """
+
+        class_time_re = re.compile(
+                r'(?P<times>0([1-9]|1[0-2]):[0-5][0-9] (P\.M\.|A\.M\.) - 0([1-9]|1[0-2]):[0-5][0-9] (P\.M\.|A\.M\.)) , (?P<days>([MWF]|Tu|Th)(,([MWF]|Tu|Th)+)*)'
+        )
+
+        class_sec_re = re.compile(r'-(?P<section>[0-9]{3}) (?P<class_type>[A-Z]{3})')
+        class_credits_re = re.compile(r'(?P<credits>)([1-9] - )?[1-9]) credits? )')
+
+        cleaned_desc = []
+        for line in desc:
+            cleaned_desc.append(
+                    re.sub(r'\s+', ' ', line.replace(u'\xa0', u' '))
+                    )
+        cleaned_desc = ''.join(cleaned_desc)
+        # now that the description is cleaned, we can do regex
+        # matching for the desired fields
+
+(?<=0([1-9]|1[0-2]):[0-5][0-9] (P\.M\.|A\.M\.) - 0([1-9]|1[0-2]):[0-5][0-9] (P\.M\.|A\.M\.) , )([MWF]|Tu|Th)(,([MWF]|Tu|Th)+)*
     def parse_schedule_form_options(self, response):
         """
         Returns a generator of FormRequest objects each containing formdata
@@ -65,10 +89,17 @@ classschedule_selectsubject.jsp?campus=UMNTC']
 
         for block in class_blocks:
             class_id = block.select('./h3/a/@name').extract()
-            class_table = block.select('.//table[@class="sectionTable"]')
-            class_table_rows = class_table.select('.//tr')
+            class_table_rows  = block.select('.//table[@class="sectionTable"]//tr')
+            #class_table_rows = class_table.select('.//tr')
             for lec in class_table_rows:
                 # get class no. , class description
+                #classNumber = a list
+                class_number = lec.select('./td[@class="classNumber"]/text()').extract()
+                #list of class instructors
+                class_instrs = lec.select(
+                        './td[@class="description"]//a[contains(@href, "www.umn.edu/lookup")]/text()'
+                        ).extract()
+
                 pass
 
         pass
